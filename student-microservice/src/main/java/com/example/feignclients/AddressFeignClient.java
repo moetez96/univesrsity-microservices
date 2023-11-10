9fbefadaf87047a9dbdba454afc8778b2c158b81
mayari <mayari@deloitte.tn>
2023-11-10 14:18:28 +0100

    update Feign Client with Eureka

--- student-microservice/src/main/java/com/example/feignclients/AddressFeignClient.java
+++ student-microservice/src/main/java/com/example/feignclients/AddressFeignClient.java
@@ -2,15 +2,11 @@ package com.example.feignclients;
 
 import com.example.response.AddressResponse;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.NoSuchElementException;
-
-@FeignClient(url="${address.webservice.url}", value = "address-feign-client",
-        path = "/api/address")
+@FeignClient(value = "address-microservice", path = "/api/address")
 public interface AddressFeignClient {
 
     @GetMapping("/getById/{id}")
