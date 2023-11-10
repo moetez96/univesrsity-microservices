d8ad459730ddcfe6c804be713359b24e3cc4a7b8
mayari <mayari@deloitte.tn>
2023-11-10 14:15:03 +0100

    configure eureka discovery clients

--- address-microservice/src/main/java/com/example/addressmicroservice/AddressMicroserviceApplication.java
+++ address-microservice/src/main/java/com/example/addressmicroservice/AddressMicroserviceApplication.java
@@ -3,6 +3,7 @@ package com.example.addressmicroservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -10,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan({"com.example.controller", "com.example.service"})
 @EntityScan("com.example.entity")
 @EnableJpaRepositories("com.example.repository")
+@EnableDiscoveryClient
 public class AddressMicroserviceApplication {
 
 	public static void main(String[] args) {
--- student-microservice/src/main/java/com/example/studentmicroservice/StudentMicroserviceApplication.java
+++ student-microservice/src/main/java/com/example/studentmicroservice/StudentMicroserviceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -15,6 +16,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 @EntityScan("com.example.entity")
 @EnableJpaRepositories("com.example.repository")
 @EnableFeignClients("com.example.feignclients")
+@EnableDiscoveryClient
 public class StudentMicroserviceApplication {
 
     @Value("${address.webservice.url}")
